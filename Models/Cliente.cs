a6ae1d6294667a89db94d634014c8f407a839e72
Priscilla Trevizan <priscillatrevizan.dev@gmail.com>
2026-01-21 21:42:15 -0300

    refactor: aplica clean code e separa responsabilidades no cálculo de pedidos, testa o programa no console interativo

---
+++ Models/Cliente.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Refatora_Calculadora.Models
+{
+    public class Cliente
+    {
+        public TipoCliente Tipo { get; }
+        public double PercentualDesconto { get; }
+
+        public Cliente(TipoCliente tipo)
+        {
+            Tipo = tipo;
+            PercentualDesconto = ObterDesconto(tipo);
+        }
+
+        private double ObterDesconto(TipoCliente tipo)
+        {
+            return tipo switch
+            {
+                TipoCliente.Regular => 0.05,
+                TipoCliente.Premium => 0.10,
+                TipoCliente.Vip => 0.15,
+                _ => 0
+            };
+        }
+    }
+}
+
---
+++ Models/TipoCliente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Refatora_Calculadora.Models
+{
+    public enum TipoCliente
+    {
+        Regular = 1,
+        Premium = 2,
+        Vip = 3
+    }
+}
+
